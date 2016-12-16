c5a0b0efe4b382dddbb0b5157bad0d8ccb5f5086
JaturongC <omega.demons.13@gmail.com>
2016-12-16 18:33:08 +0700

    Add 1 interface -> NotificationService.java and 1 method -> NotificationServiecImpl.java

--- src/main/java/blog/jaturong/services/NotificationServiceImpl.java
+++ src/main/java/blog/jaturong/services/NotificationServiceImpl.java
@@ -1,9 +1,16 @@
 package blog.jaturong.services;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpSession;
+
 public class NotificationServiceImpl implements NotificationService {
 
+    public static final String NOTIFY_MSG_SESSION_KEY = "siteNotificationMessages";
 
+    @Autowired
+    private HttpSession httpSession;
 
     @Override
     public void addInfoMessage(String msg) {
