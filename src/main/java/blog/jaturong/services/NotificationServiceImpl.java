b8a13442b867a2a2e46864b932e97b8f00972354
JaturongC <omega.demons.13@gmail.com>
2016-12-16 18:28:14 +0700

    Add 1 interface -> NotificationService.java and 1 method -> NotificationServiecImpl.java

---
+++ src/main/java/blog/jaturong/services/NotificationServiceImpl.java
@@ -0,0 +1,17 @@
+package blog.jaturong.services;
+
+
+public class NotificationServiceImpl implements NotificationService {
+
+
+
+    @Override
+    public void addInfoMessage(String msg) {
+
+    }
+
+    @Override
+    public void addErrorMessage(String msg) {
+
+    }
+}
